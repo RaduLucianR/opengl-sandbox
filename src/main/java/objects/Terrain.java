2b589fbde8b044a47ba6ae7d11e28bf85d0019fe
TeodorLungu <lungu.teodorc@gmail.com>
2020-10-15 22:10:40 +0300

    Phong attempt si terrain
    commit

--- src/main/java/objects/Terrain.java
+++ src/main/java/objects/Terrain.java
@@ -6,6 +6,10 @@ import nl.tue.s2iv60.core.util.Material;
 import nl.tue.s2iv60.core.cg.Renderable;
 import org.joml.Vector3f;
 import static sandbox.Sandbox.CheckBoxID.SHOWTERRAIN;
+import static sandbox.Sandbox.ComboBoxID.VIEWMODE;
+import static sandbox.Sandbox.SliderID.SLIDER1;
+import static sandbox.Sandbox.SliderID.SLIDER2;
+
 import shaders.ShaderPrograms;
 
 public class Terrain implements Renderable {
@@ -15,9 +19,51 @@ public class Terrain implements Renderable {
 
     public Terrain() {
     }
-
+    private float rtri =0.0f;
     @Override
     public void render(GL2 gl, GLUT glut, double tAnim, double dt) {
-        if (!SHOWTERRAIN.getValue()) return;
+        ShaderPrograms.useDefaultShader(gl);
+
+        gl.glClearDepth( 1.0f );
+        gl.glEnable( gl.GL_DEPTH_TEST ); // Enable Depth Test to make shape solid
+        gl.glDepthFunc( gl.GL_LEQUAL );
+        gl.glHint( gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST );  // Clear The Screen And The Depth Buffer
+        gl.glClear( gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT );
+
+        gl.glPushMatrix();
+        gl.glBegin( gl.GL_QUADS ); // Start Drawing The Cube
+        gl.glColor3f( 0,1,0 );   //green color
+        gl.glVertex3f( 25.0f, 25.0f, -1.0f ); // Top Right Of The Quad (Top)
+        gl.glVertex3f( -25.0f, 25.0f, -1.0f); // Top Left Of The Quad (Top)
+        gl.glVertex3f( -25.0f, 25.0f, 1.0f ); // Bottom Left Of The Quad (Top)
+        gl.glVertex3f( 25.0f, 25.0f, 1.0f ); // Bottom Right Of The Quad (Top)
+        gl.glColor3f( 0f,1f,0f ); //green color
+        gl.glVertex3f( 25.0f, -25.0f, 1.0f ); // Top Right Of The Quad
+        gl.glVertex3f( -25.0f, -25.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( -25.0f, -25.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 25.0f, -25.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 0f,0f,1f ); //blue color
+        gl.glVertex3f( 25.0f, 25.0f, 1.0f ); // Top Right Of The Quad (Front)
+        gl.glVertex3f( -25.0f, 25.0f, 1.0f ); // Top Left Of The Quad (Front)
+        gl.glVertex3f( -25.0f, -25.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 25.0f, -25.0f, 1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 0f,0f,1f ); //blue color
+        gl.glVertex3f( 25.0f, -25.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -25.0f, -25.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glVertex3f( -25.0f, 25.0f, -1.0f ); // Top Right Of The Quad (Back)
+        gl.glVertex3f( 25.0f, 25.0f, -1.0f ); // Top Left Of The Quad (Back)
+        gl.glColor3f( 1f,0f,0f ); //red color
+        gl.glVertex3f( -25.0f, 25.0f, 1.0f ); // Top Right Of The Quad (Left)
+        gl.glVertex3f( -25.0f, 25.0f, -1.0f ); // Top Left Of The Quad (Left)
+        gl.glVertex3f( -25.0f, -25.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -25.0f, -25.0f, 1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 1f,0f, 0f ); //red color
+        gl.glVertex3f( 25.0f, 25.0f, -1.0f ); // Top Right Of The Quad (Right)
+        gl.glVertex3f( 25.0f, 25.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( 25.0f, -25.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 25.0f, -25.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glEnd(); // Done Drawing The Quad
+        gl.glFlush();
+        gl.glPopMatrix();
     }
 }
