c0d592aafc4eb3bc09ec0c81ba9ed323f32b841d
Lucius29 <radlucradulescu@gmail.com>
2020-10-16 13:21:50 +0200

    Updated FlyingCamera. FlyingCamera is now complete

--- src/main/java/sandbox/FlyingCamera.java
+++ src/main/java/sandbox/FlyingCamera.java
@@ -20,40 +20,41 @@ class FlyingCamera extends Camera {
      */
     public FlyingCamera(Vector3f e, Vector3f c, Vector3f u) {
         boolean useGiven = e!=null && c!=null && u!=null;
-        this.eye    = useGiven? e: new Vector3f(10,10,10);
+        this.eye    = useGiven? e: new Vector3f(10,-10,10);
         this.center = useGiven? c: new Vector3f(0,0,0);
         this.up     = useGiven? u: new Vector3f(0,0,1);
 
-        Vector3f vDistance = eye.sub(center);
-        GS.vDist = vDistance.length();
+        Vector3f vDistance = eye.sub(center); //Compute the eye-vector
+        GS.vDist = vDistance.length(); //vDist is the length of the eye-vector
 
-        Vector3f x = new Vector3f(1,0,0);
-        Vector3f y = new Vector3f(0,1,0);
-        Vector3f z = new Vector3f(0,0,1);
+        Vector3f x = new Vector3f(1,0,0); //Versor for direction x
+        Vector3f y = new Vector3f(0,1,0); //Versor for direction y
+        Vector3f z = new Vector3f(0,0,1); //Versor for direction z
 
-        Vector3f normalXY = new Vector3f(0);
-        Vector3f normalXZ = new Vector3f(0);
+        Vector3f normalXY = new Vector3f(0); //Normal of plan XY
+        x.cross(y,normalXY); //Find the normal of plan XY and store it in normalXY
+        float dotXY = eye.dot(normalXY); //Find the dot product of the normal of plane XY and the eye-vector
+        normalXY.mul(dotXY); //The aforementioned dot product represents the length of the normal of plane XY,
+                             //hence apply it to normalXY
+        Vector3f projXY = new Vector3f(0); //The projection of the eye-vector on plane XY
+        eye.sub(normalXY,projXY); //Compute the projection-vector of the eye-vector
 
-        x.cross(y,normalXY);
-        x.cross(z,normalXZ);
+        float quadrantAdjustment = 0; //Adjust angle theta based on its quadrant
+        if (eye.x > 0 && eye.y < 0) { //Adjust based on quadrant IV
+            quadrantAdjustment = (float) (- PI/2);
+        }
+        if (eye.x < 0 && eye.y < 0) { //Adjust based on quadrant III
+            quadrantAdjustment = (float) (+ PI/2);
+        }
+        GS.theta = x.angle(projXY) + quadrantAdjustment; //Calculate theta, which is the angle between the
+                                                         //projection-vector and the x versor
 
-        float dotXY = eye.dot(normalXY);
-        float dotXZ = eye.dot(normalXZ);
-
-        normalXY.mul(dotXY);
-        normalXZ.mul(dotXZ);
-
-        Vector3f projXY = new Vector3f(0);
-        Vector3f projXZ= new Vector3f(0);
-
-        eye.sub(normalXY,projXY);
-        eye.sub(normalXZ,projXZ);
-
-        GS.theta = x.angle(projXY);
-        GS.phi = x.angle(projXZ);
-
-
-        //  compute theta, phi, and vDist to start with same eye point and direction
+        //Calculate phi, which is the angle between the eye-vector and plan XY
+        if (eye.z > 0) { //Adjust angle phi based on whether the eye-vector is above or below the XY plane
+            GS.phi = eye.angle(projXY);
+        } else {
+            GS.phi = - eye.angle(projXY);
+        }
     }
 
     @Override
@@ -61,13 +62,14 @@ class FlyingCamera extends Camera {
         // Compute eye vector based on center and GS.theta, GS.phi, and GS.vDist.
         // Keep up-vector the same.
 
-        Vector3f z = new Vector3f(0,0,1);
-        Vector3f y = new Vector3f(0,1,0);
-        Vector3f eyePosition = new Vector3f(1,0,0);
-        eyePosition.rotateZ(GS.theta);
-        eyePosition.rotateY(-GS.phi);
-        eyePosition.mul(GS.vDist);
-        eye = eyePosition;
+        float cosinusP = (float) Math.cos(GS.phi); //Cosinus of angle phi
+        float cosinusT = (float) Math.cos(GS.theta);//Cosinus of angle theta
+        float sinusP = (float) Math.sin(GS.phi); //Sinus of angle phi
+        float sinusT = (float) Math.sin(GS.theta); //Cosinus of angle theta
+        float x = GS.vDist * cosinusP * cosinusT; //Compute x coordinate of the eye-vector
+        float y = GS.vDist * cosinusP * sinusT; //Compute y coordinate of the eye-vector
+        float z = GS.vDist * sinusP; //Compute z coordinate of the eye-vector
+        eye = new Vector3f(x,y,z);
         super.apply(gl, glu);
     }
 }
