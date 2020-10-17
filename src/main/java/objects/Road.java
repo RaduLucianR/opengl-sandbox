b9fcb7eb42136f7549cee405edd505d77e468c31
Lucius29 <radlucradulescu@gmail.com>
2020-10-17 02:05:36 +0200

    The Bezier construction technique is complete.

--- src/main/java/objects/Road.java
+++ src/main/java/objects/Road.java
@@ -41,9 +41,60 @@ public class Road implements Renderable {
 
     @Override
     public void render(GL2 gl, GLUT glut, double tAnim, double dt) {
-        int numControlPoints = 5;
         if (SHOWPATH.getValue()) {
+            gl.glColor3f(1,0,0);
+            float t = 0.0f;
+            Vector3f A = new Vector3f(20,20,0.0001f);
+            Vector3f B = new Vector3f(-20,20,0.0001f);
+            Vector3f C = new Vector3f(20,-20,0.0001f);
+            Vector3f D = new Vector3f(-20,-20,0.0001f);
 
+            float[] axisX = new float[1001];
+            float[] axisY = new float[1001];
+
+            int  i = 0;
+            while(true)
+            {
+                if(t > 1.0f) break;
+
+                axisX[i] = (float) bezier(A.x,B.x,C.x,D.x,t);
+                axisY[i] = (float) bezier(A.y,B.y,C.y,D.y,t);
+
+                i++;
+
+                t += 0.01f;
+            }
+
+            float width = 3f;
+            Vector3f du = new Vector3f(0);
+            Vector3f dv = new Vector3f(0);
+            Vector3f point = new Vector3f(0);
+            Vector3f pointPrev = new Vector3f(0);
+
+            gl.glBegin(GL2.GL_QUAD_STRIP);
+            for (int j = 1; j < i; j++) {
+                point.x = axisX[j];
+                point.y = axisY[j];
+                pointPrev.x = axisX[j-1];
+                pointPrev.y = axisY[j-1];
+                point.sub(pointPrev,du);
+                dv.x = du.y;
+                dv.y = -du.x;
+                dv.mul(width/(2.0f*dv.length()));
+                Vector3f p0 = new Vector3f(0);
+                Vector3f p1 = new Vector3f(0);
+                Vector3f p2 = new Vector3f(0);
+                Vector3f p3 = new Vector3f(0);
+
+                point.sub(dv,p0);
+                point.add(dv,p1);
+                pointPrev.add(dv,p2);
+                pointPrev.sub(dv,p3);
+
+                gl.glVertex3f(p0.x,p0.y,0.001f);
+                gl.glVertex3f(p1.x,p1.y,0.001f);
+            }
+            gl.glEnd();
         }
     }
 
